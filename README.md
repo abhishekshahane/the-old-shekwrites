f3a76331f5c9c5b5f6112746721679957e1f91b8
Abhishek <nintendo.spy123@gmail.com>
2020-07-27 11:44:37 +0000

    Added some more JS and styling

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# shekwrites
-The official code for the website shekwrites. Also contributing on this project is one of the best developers I know, @delivey! I will be mainly coding this website, along with him.
+# ShekWrites
+The official code for the website shekwrites. Also contributing on this project is one of the best developers I know, @delivey! I will be mainly coding this website, along with him. We hope to deploy it soon! It was made using the languages HTML, CSS, Python and a little bit of JavaScript.
+<br></br>
+Enjoy:) -
+Abhishek
--- static/main.js
+++ static/main.js
@@ -1,5 +1,56 @@
-alert("Hello, and welcome to ShekWrites!");
-    
+var b = 1;
+function greet()
+{
+    // Greet only once.
+    while (b == 1){
+        alert("Hello, and welcome to ShekWrites!");
+        b--;
+    }
+}
+// Function for future use.
 function nav() {
     document.getElementsByClassName("nav").style.width = "250px";
+}
+var n = 1;
+var count = 0;
+function a()
+{
+    if (n == 1){
+        var seconds = 30;
+        var countdown = setInterval(function() {
+            seconds--;
+            document.getElementById("countdown").textContent = "You have " + seconds + " seconds left, hurry!";
+            if (seconds <= 0) clearInterval(countdown);
+            if(seconds == 0)
+            {
+                // You can find the average per second by dividing the user's clicks by the seconds.
+                // In this case, the number of seconds were thirty.
+                var average = count / 30;
+                if (count > 120){
+                    alert("Wow! You are pretty fast!")
+                    alert("Thanks for playing the click test, your score was " + count, + " Share it!");
+                    alert("Also, Your average was " + average + " CPS.");
+                }
+                else if (count > 100 && count < 120)
+                {
+                    alert("Not bad, but you could improve.");
+                    alert("Thanks for playing the click test, your score was " + count, + " Share it!");
+                    alert("Also, Your average was " + average + " CPS.");
+                }
+                else{
+                    alert("You can do better, try again by refreshing the page.");
+                    alert("Thanks for playing the click test, your score was " + count, + " Share it!");
+                    alert("Also, Your average was " + average + " CPS.");
+                }
+        }
+        }, 1000);
+        n--;
+    }
+    count++;
+    /*
+    We are only going to run the timer function when
+    n is clicked the first time, so it doesn't run
+    for every single click.
+    */
+    document.getElementById("count").textContent = count + " clicks";
 }
